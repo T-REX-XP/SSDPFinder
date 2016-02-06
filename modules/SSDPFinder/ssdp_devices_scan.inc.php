b6594098d0bda7cee679c7ed94738637b05f677b
T-REX-XP <T-REX-XP@ya.ru>
2016-02-06 22:59:55 +0200

    initial
    initial commit.
    Worked: Only scan UPNP devices

---
+++ modules/SSDPFinder/ssdp_devices_scan.inc.php
@@ -0,0 +1,118 @@
+<?php
+/*
+* @version 0.1 (wizard)
+*/
+require('upnp/vendor/autoload.php');
+use jalder\Upnp\Upnp;
+global $session;
+if ($this->owner->name=='panel') {
+    $out['CONTROLPANEL']=1;
+}
+$qry="1";
+// search filters
+// QUERY READY
+global $save_qry;
+if ($save_qry) {
+    $qry=$session->data['ssdp_devices_qry'];
+} else {
+    $session->data['ssdp_devices_qry']=$qry;
+}
+if (!$qry) $qry="1";
+//="ID DESC";
+//$out['SORTBY']=$sortby_ssdp_devices;
+// SEARCH RESULTS
+$res= Scan();
+if ($res[0]['ID']) {
+    //paging($res, 100, $out); // search result paging
+    $total=count($res);
+    for($i=0;$i<$total;$i++) {
+        // some action for every record if required
+        $tmp=explode(' ', $res[$i]['UPDATED']);
+        $res[$i]['UPDATED']=fromDBDate($tmp[0])." ".$tmp[1];
+    }
+    $out['RESULT']=$res;
+}
+
+function Scan()
+{
+    $upnp = new Upnp();
+    print('searching...'.PHP_EOL);
+    $everything = $upnp->discover();
+    $result=[];
+
+    foreach($everything as $device){
+        //print_r($device);  //uncomment to see all available array elements for a device.
+
+        $info = $device['description']['device'];
+
+       // print array_search(, array_column( $result, 'ADDRESS'));
+        if(!array_search_result($result,'UUID',$info["UDN"]))
+        {
+            $result[] =  [
+                "ID" => $info["UDN"],
+                "TITLE" => $info["friendlyName"],
+                "ADDRESS" => $info["presentationURL"],
+                "UUID" => $info["UDN"],
+                "DESCRIPTION" => $info["modelDescription"],
+                "TYPE" =>explode(":", $info["deviceType"])[3] ,
+                "LOGO" => getDefImg($info),
+                "SERIAL" => $info["serialNumber"],
+                "MANUFACTURERURL" => $info["manufacturerURL"],
+                "UPDATED" => '',
+                "MODEL" => $info["modelName"],
+                "MANUFACTURER" => $info["manufacturer"],
+                "IP" =>explode(":", $info["presentationURL"])[1] ,
+            ];
+        }
+    }
+    /*
+    print("<pre>");
+    print_r($result);
+     print("</pre>");
+    */
+    return $result;
+}
+
+function array_search_result($array,$key,$value)
+{
+  //  global $result;
+    foreach($array as $k=>$v)
+    {
+        if(array_key_exists($key,$v) && ($v[$key] == $value))
+        {
+          return true;
+        }
+    }
+   // return $result;;
+}
+
+function getDefImg($dev)
+{
+//print ("<pre>DIR: " .DIR_MODULES.$this->name );
+    if($dev["manufacturer"]== "Google Inc." && $dev["modelName"] =="Eureka Dongle")
+    {
+        return  "/templates/SSDPFinder/img/chromecast.png";
+    }
+    if(($dev["manufacturer"]== "LG Electronics." || $dev["manufacturer"]== "LG Electronics") && ($dev["modelName"] =="LG TV" || $dev["modelName"] =="LG Smart TV"))
+    {
+        return "/templates/SSDPFinder/img/tv.png";
+    }
+    if($dev["manufacturer"]== "Synology" || $dev["manufacturer"]=="Synology Inc")
+    {
+        return "/templates/SSDPFinder/img/synology.png";
+    }
+    if($dev["manufacturer"]== "Emby" && $dev["modelName"] == "Emby")
+    {
+        return $dev["presentationURL"] . $dev["iconList"]["icon"]["4"]["url"];
+    }
+    if($dev["manufacturer"]== "Linksys" || $dev["manufacturer"] == "Cisco")
+    {
+        return "/templates/SSDPFinder/img/router.png";
+    }
+  //  return $result;
+}
+
+
+
+
+
---
+++ modules/SSDPFinder/ssdp_devices_search.inc.php
@@ -0,0 +1,46 @@
+<?php
+/*
+* @version 0.1 (wizard)
+*/
+require('upnp/vendor/autoload.php');
+use jalder\Upnp\Upnp;
+ global $session;
+  if ($this->owner->name=='panel') {
+   $out['CONTROLPANEL']=1;
+  }
+  $qry="1";
+  // search filters
+  // QUERY READY
+  global $save_qry;
+  if ($save_qry) {
+   $qry=$session->data['ssdp_devices_qry'];
+  } else {
+   $session->data['ssdp_devices_qry']=$qry;
+  }
+  if (!$qry) $qry="1";
+  $sortby_ssdp_devices="ID DESC";
+  $out['SORTBY']=$sortby_ssdp_devices;
+  // SEARCH RESULTS
+  $res= SQLSelect("SELECT * FROM ssdp_devices WHERE $qry ORDER BY ".$sortby_ssdp_devices);
+  if ($res[0]['ID']) {
+   //paging($res, 100, $out); // search result paging
+   $total=count($res);
+   for($i=0;$i<$total;$i++) {
+    // some action for every record if required
+    $tmp=explode(' ', $res[$i]['UPDATED']);
+    $res[$i]['UPDATED']=fromDBDate($tmp[0])." ".$tmp[1];
+   }
+   /*
+   print("<pre>");
+   print_r($res);
+   print("</pre>");
+*/
+   $out['RESULT']=$res;
+  }
+
+
+
+
+
+
+
