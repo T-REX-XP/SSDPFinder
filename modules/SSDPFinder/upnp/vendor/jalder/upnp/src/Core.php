f86025392c7ef490c6f048c1abcc8bf529bc2d03
tarasfrompir <tarasfrompir@inbox.ru>
2018-02-21 10:03:49 +0200

    Update Core.php
    Иногда при пустом списке устройств показывает ВОРНИНГ
    Дабы этого избежать рекомендую это изменение

--- modules/SSDPFinder/upnp/vendor/jalder/upnp/src/Core.php
+++ modules/SSDPFinder/upnp/vendor/jalder/upnp/src/Core.php
@@ -30,7 +30,9 @@ class Core {
         $response = array();
         do {
             $buf = null;
-            socket_recvfrom($socket, $buf, 1024, MSG_WAITALL, $from, $port);
+            if ($len = @socket_recvfrom($socket, $buf, 1024, MSG_WAITALL, $from, $port) == -1) {
+	        echo "socket read failed: " ;
+	        }
             if(!is_null($buf)){
                 $data = $this->parseSearchResponse($buf);
                 $response[$data['usn']] = $data;
