3bb6ae2ac794b1ac2b7df782dd754ba693c079d7
T-REX-XP <4560084+T-REX-XP@users.noreply.github.com>
2018-02-25 03:22:46 +0200

    Added  info about services of device

--- modules/SSDPFinder/ssdp_devices_scan.inc.php
+++ modules/SSDPFinder/ssdp_devices_scan.inc.php
@@ -23,7 +23,7 @@ if (!$qry) $qry = "1";
 //$out['SORTBY']=$sortby_ssdp_devices;
 // SEARCH RESULTS
 $res = Scan();
-if ($res[0]['ID']) {
+if ($res[0]['UUID']) {
     //paging($res, 100, $out); // search result paging
     $total = count($res);
     for ($i = 0; $i < $total; $i++) {
@@ -49,7 +49,7 @@ function Scan()
         // print array_search(, array_column( $result, 'ADDRESS'));
         if (!array_search_result($result, 'UUID', $info["UDN"]) && !is_null($info["UDN"])) {
             $result[] = [
-                "ID" => $info["UDN"],
+              //  "ID" => $info["UDN"],
                 "TITLE" => $info["friendlyName"],
                 "ADDRESS" => $info["presentationURL"],
                 "UUID" => $info["UDN"],
@@ -62,6 +62,7 @@ function Scan()
                 "MODEL" => $info["modelName"],
                 "MANUFACTURER" => $info["manufacturer"],
                 "IP" => getIp($info),
+                "SERVICES"=> getServices($info),
             ];
         }
     }
@@ -91,16 +92,79 @@ function getIp($dev)
     return str_replace("//", "", $result);
 }
 
+function startsWith($haystack, $needle)
+{
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
+}
+
+function getServices($dev){
+	$services = $dev["serviceList"]["service"];
+	$result = array();
+	if($services){
+		$servType = $services["serviceType"];
+		//print "Serv Type: " . $servType;
+		if(startsWith($servType,"urn") ){ // && empty($servType)
+			//print "First cond->";
+			$name = explode(":", $servType)[3];
+			array_push($result,$name);
+		}
+		else{
+		
+			foreach($services as $k => $v){
+					$value = $v["serviceType"];
+				
+						$name = explode(":", $value)[3];
+					
+						array_push($result,$name);
+					}
+			}
+	}
+	return implode(",",$result);
+}
+
+function endsWith($haystack, $needle)
+{
+	return $needle === '' || substr_compare($haystack, $needle, -strlen($needle)) === 0;
+}
+
+function SearchArray($array, $searchIndex, $searchValue)
+{
+    if (!is_array($array) || $searchIndex == '')
+        return false;
+
+    foreach ($array as $k => $v)
+    {
+        if (is_array($v) && array_key_exists($searchIndex, $v) && $v[$searchIndex] == $searchValue)
+            return $k;
+    }
+
+    return false;
+}
+
 function getDefImg($dev)
 {
-$result = $dev["presentationURL"] . $dev["iconList"]["icon"]["0"]["url"];
-if($result) {
-    return $dev["presentationURL"] . $dev["iconList"]["icon"]["0"]["url"];
-}   
-$result = "/templates/SSDPFinder/img/".$dev["manufacturer"].".png";
-    if (!$result) {
-        return "/templates/SSDPFinder/img/unknow.png";
-    }else  {
-     return $result;
-    }
+	$baseUrl = $dev["presentationURL"];
+	if( !empty($baseUrl) && endsWith($baseUrl,"/")){
+		$baseUrl=  rtrim($baseUrl,"/");//$baseUrl . ;
+	}
+
+	if( $dev["iconList"]["icon"]){
+		$icons = $dev["iconList"]["icon"];
+
+		$searchedValue = 48; // Value to search.
+		$index48 = SearchArray($icons,"width",48);
+
+		$img48 =""; //empty by def
+		if($index48 !=false){
+			$img48 = $icons[$index48]["url"];
+		}else{
+			$img48 = $icons[0]["url"];
+		}
+		return $baseUrl . $img48;
+		
+	}else{
+        $type =explode(":", $dev["deviceType"])[3];
+		return "/templates/SSDPFinder/img/".$type. ".png";//"Icons not found... (((";
+	}
 }
