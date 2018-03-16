7dd480a1f54bf08fd1d821eca66738f00d44c969
T-REX-XP <4560084+T-REX-XP@users.noreply.github.com>
2018-03-17 00:54:44 +0200

    remove ip property

--- modules/ssdp_finder/ssdp_finder.class.php
+++ modules/ssdp_finder/ssdp_finder.class.php
@@ -261,7 +261,7 @@ function usual(&$out) {
   $pinghosts['TYPE'] = '0';
   $pinghosts['OFFLINE_INTERVAL'] = '600';
   $pinghosts['ONLINE_INTERVAL'] = '600';
-  $pinghosts['HOSTNAME'] = $this->getIp($ssdpdevice['IP']);
+  $pinghosts['HOSTNAME'] = $this->getIp($ssdpdevice['ADDRESS']);
   $pinghosts['CODE_ONLINE'] = 'say("Устройство ".$host[\'TITLE\']." пропало из сети, возможно его отключили" ,2);';
   $pinghosts['CODE_OFFLINE'] = 'say("Устройство ".$host[\'TITLE\']." появилось в сети." ,2);';
   $pinghosts['LINKED_OBJECT'] = $ssdpdevice['LINKED_OBJECT'];
