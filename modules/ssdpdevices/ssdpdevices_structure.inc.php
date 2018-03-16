d894770be4706c91b85325691540e67fe387b727
tarasfrompir <tarasfrompir@inbox.ru>
2018-03-16 13:58:25 +0200

    Add files via upload

--- modules/ssdpdevices/ssdpdevices_structure.inc.php
+++ modules/ssdpdevices/ssdpdevices_structure.inc.php
@@ -21,10 +21,13 @@ $this->ssdpdevices_types=array(
         'PARENT_CLASS'=>'UPNPdevices',
         'CLASS'=>'SMediaServer',
         'PROPERTIES'=>array(
-	    'mute'=>array('DESCRIPTION'=>'Отключение/включение звука', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'mute-unmute', 'DATA_KEY'=>1),
+	    'mute_unmute'=>array('DESCRIPTION'=>'Отключение/включение звука', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'mute-unmute', 'DATA_KEY'=>1),
+            'pause_unpause'=>array('DESCRIPTION'=>'Отключение/включение паузы', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'pause-unpause', 'DATA_KEY'=>1),
        ),
     'METHODS'=>array(
             'mute-unmute'=>array('DESCRIPTION'=>'Отключение/включение звука'),
+            'pause-unpause'=>array('DESCRIPTION'=>'Отключение/включение паузы'),
+
         ),
     ),
     'dial'=>array(
