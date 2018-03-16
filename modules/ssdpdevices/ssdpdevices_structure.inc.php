45e3119c9ae18ff32d60dafca484ce8b07d59e1c
tarasfrompir <tarasfrompir@inbox.ru>
2018-03-16 14:17:36 +0200

    Add files via upload

--- modules/ssdpdevices/ssdpdevices_structure.inc.php
+++ modules/ssdpdevices/ssdpdevices_structure.inc.php
@@ -58,7 +58,6 @@ $this->ssdpdevices_types=array(
             'Password'=>array('DESCRIPTION'=>'Password','ONCHANGE'=>'updatePreview','_CONFIG_TYPE'=>'text'),
        ),
      ),
-
     'InternetGatewayDevice'=>array(
         'TITLE'=>'UPNP Роутер',
         'PARENT_CLASS'=>'UPNPdevices',
@@ -66,6 +65,13 @@ $this->ssdpdevices_types=array(
         'PROPERTIES'=>array(
             'Username'=>array('DESCRIPTION'=>'Username','_CONFIG_TYPE'=>'text'),
             'Password'=>array('DESCRIPTION'=>'Password','ONCHANGE'=>'updatePreview','_CONFIG_TYPE'=>'text'),
+        ),
+    ),
+    'MediaRenderer'=>array(
+        'TITLE'=>'UPNP Телевизор',
+        'PARENT_CLASS'=>'UPNPdevices',
+        'CLASS'=>'SMediaRenderer',
+        'PROPERTIES'=>array(
         ),
     ),
 );
