a6aa4cbca4af04e919e6ffe2bb529a154b4c4c91
tarasfrompir <tarasfrompir@inbox.ru>
2018-03-27 16:50:27 +0300

    Update SMediaRenderer_mute-unmute.php

--- modules/ssdpdevices/SMediaRenderer_mute-unmute.php
+++ modules/ssdpdevices/SMediaRenderer_mute-unmute.php
@@ -1,28 +1,15 @@
 <?php
 
-require(dirname(__FILE__).'/../ssdp_finder/upnp/vendor/autoload.php');
-
-use jalder\Upnp\Renderer;
-
-$renderer = new Renderer();
-
-$renderers = $renderer->discover();
-
-if(!count($renderers)){
-    print_r('no upnp renderers found'.PHP_EOL);
-}
 $uuid = $this->getProperty("UUID");
 $mute_unmute = $this->getProperty("mute_unmute");
-foreach($renderers as $r){
-    $remote = new Renderer\Remote($r);
-    if ( $mute_unmute AND $uuid == $r['description']['device']['UDN']) {
-            $result = $remote->mute();
-            
-        } else {
-            $result = $remote->unmute();
-        }
-echo $result;
+
+require(dirname(__FILE__).'/../ssdp_finder/upnp/vendor/autoload.php');
+use jalder\Upnp\Renderer;
+$renderer = new Renderer();
+$renderers = $renderer->discover_uuid($uuid);
+$remote = new Renderer\Remote($renderers);
+if ( $mute_unmute AND $uuid == $r['description']['device']['UDN']) {
+   $result = $remote->mute();
+} else {
+   $result = $remote->unmute();
 }
-
-
-
