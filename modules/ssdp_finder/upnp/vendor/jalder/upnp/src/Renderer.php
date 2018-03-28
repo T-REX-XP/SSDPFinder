7b40d63e526cea5e996cc9e7e71ba4cdf3ccfc17
tarasfrompir <tarasfrompir@inbox.ru>
2018-03-28 08:35:59 +0300

    Add files via upload

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Renderer.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Renderer.php
@@ -10,9 +10,6 @@ class Renderer extends Core
        if ($type = 'AVTransport') {
         return parent::search('urn:schemas-upnp-org:service:AVTransport:1');
        }
-       if ($type = 'RenderingControl') {
-        return parent::search('urn:schemas-upnp-org:service:RenderingControl:1');
-       }
     }
     /**
      * if a previous ran upnp core search is available in memory, just filter for the renderers
@@ -24,9 +21,6 @@ class Renderer extends Core
         if(is_array($results)){
             foreach($results as $usn=>$device){
                 if($device['st'] !== 'urn:schemas-upnp-org:service:AVTransport:1'){
-                    unset($results[$usn]);
-                }
-                if($device['st'] !== 'urn:schemas-upnp-org:service:RenderingControl:1'){
                     unset($results[$usn]);
                 }
             }
