e42f80ad5fc1b26a4bcd037982b6623cb33157b9
tarasfrompir <tarasfrompir@inbox.ru>
2018-04-06 22:20:10 +0300

    Add files via upload

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/examples/upnp.renderer.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/examples/upnp.renderer.php
@@ -4,21 +4,9 @@ require(dirname(__FILE__).'/../../../autoload.php');
 
 use jalder\Upnp\Renderer;
 
-$renderer = new Renderer();
+$renderer = new Renderer(); 
+$remote = new Renderer\Remote('http://192.168.100.110:1370/');
+$result = $remote->getMedia();
+print_r($result);
 
-print('searching...'.PHP_EOL);
-
-$renderers = $renderer->discover();
-$movie = 'https://ia700408.us.archive.org/26/items/BigBuckBunny_328/BigBuckBunny_512kb.mp4';
-
-if(!count($renderers)){
-    print_r('no upnp renderers found'.PHP_EOL);
-}
-
-foreach($renderers as $r){
-    print($r['description']['device']['friendlyName']);
-    $remote = new Renderer\Remote($r);
-    $result = $remote->play($movie);
-    print_r($result);
-}
 
