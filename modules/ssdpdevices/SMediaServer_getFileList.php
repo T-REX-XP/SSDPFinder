446930f7135b15e95484068533ad9a87c84867b9
tarasfrompir <tarasfrompir@inbox.ru>
2018-05-02 16:24:29 +0300

    Исправлено создание поля для Типов файлов

--- modules/ssdpdevices/SMediaServer_getFileList.php
+++ modules/ssdpdevices/SMediaServer_getFileList.php
@@ -3,6 +3,10 @@
 require(dirname(__FILE__).'/../ssdp_finder/upnp/vendor/autoload.php');
 
 use jalder\Upnp\Mediaserver;
+// массивы для определения типов файлов
+$video = [".3g2",".3gp",".3gp2",".3gpp",".3gpp2",".asf",".asx",".avi",".bin",".dat",".drv",".f4v",".flv",".gtp",".h264",".m4v",".mkv",".mod",".moov",".mov",".mp4",".mpeg",".mpg",".mts",".rm",".rmvb",".spl",".srt",".stl",".swf",".ts",".vcd",".vid",".vid",".vid",".vob",".webm",".wm",".wmv",".yuv"];
+$audio = [".aac",".ac3",".aif",".aiff",".amr",".aob",".ape",".asf",".aud",".aud",".aud",".aud",".awb",".bin",".bwg",".cdr",".flac",".gpx",".ics",".iff",".m",".m3u",".m3u8",".m4a",".m4b",".m4p",".m4r",".mid",".midi",".mod",".mp3",".mp3",".mp3",".mpa",".mpp",".msc",".msv",".mts",".nkc",".ogg",".ps",".ra",".ram",".sdf",".sib",".sln",".spl",".srt",".srt",".temp",".vb",".wav",".wav",".wave",".wm",".wma",".wpd",".xsb",".xwb"];
+
 
 $adress = $this->getProperty("CONTROLADDRESS");
 
@@ -20,7 +24,14 @@ foreach($directories as $list){
         $Record['URL_LINK'] = $file ['link'];
         $Record['TITLE'] = $file ['title'];
         $Record['DESCRIPTION'] = $file ['creator'];
-        $Record['GENRE'] = $file ['genre'];
+        $ext_file = substr(strrchr($file ['link'], "."),0);
+        if (in_array($ext_file, $video)) {
+            $Record['GENRE'] = 'Видео';
+            }else if (in_array($ext_file, $audio)) {
+            $Record['GENRE'] = 'Аудио';
+            }else {
+            $Record['GENRE'] = 'Изображения';
+            };
         $Record['LINKED_OBJECT'] = $this->description;
         SQLUpdateInsert('mediaservers_playlist', $Record);
     }
