16b2546d293f616992c3efca31d7c009a937b35e
tarasfrompir <tarasfrompir@inbox.ru>
2018-05-04 10:23:54 +0300

    Исправлен стиль оформления

--- modules/ssdpdevices/medaiserver.urllist.php
+++ modules/ssdpdevices/medaiserver.urllist.php
@@ -6,14 +6,12 @@ $DB->set_charset("utf8");
 // selectType - tip faila
 // selectServers - imya servera
 // selectName - imya faila
-
 switch ($_POST['action']){
-
         case "SERVERS":
 		//  сортируме названия серверов 
 		$rows = $DB->query('SELECT * FROM mediaservers_playlist WHERE GENRE LIKE"'.$_POST['types'].'"');
                 $chekedserver = [];
-                echo '<select size="1" name="selectServer" onClick="document.getElementById('."'".'selectedname'."'".').value='."'".' '."'".'; return false;" onchange="selectNames()" style="float:left;">';
+                echo '<select size="1" name="selectServer" onClick="document.getElementById('."'".'selectedname'."'".').value='."''".'; return false;" onchange="selectNames();selectUrl();" style="width:175px; text-align:center; font-family: monospace; ">';
                 foreach ($rows as $numRow => $row) {
                    if (!in_array($row['LINKED_OBJECT'], $chekedserver)){
                        echo '<option value="'.$row['LINKED_OBJECT'].'">'.$row['LINKED_OBJECT'].'</option>';
@@ -23,9 +21,7 @@ switch ($_POST['action']){
 		echo '</optgroup>';
                 echo '</select>';
                 echo '</div>';
- 
                 return;
-
         case "NAMES":
 		echo '<datalist id="names" name="selected_url" >';
 		//  сортируме названия names 
@@ -35,20 +31,16 @@ switch ($_POST['action']){
 		};
 		echo '</datalist>';
                 return;
-
         case "URL":
 		//  сортируме названия url
-
 		$rows = $DB->query('SELECT * FROM mediaservers_playlist WHERE TITLE="'.$_POST['name'].'" LIMIT 1');   
                 foreach ($rows as $numRow => $row) {
                       if ($row['URL_LINK']){
-		        echo '<input type="text" id="play_url" value="'.$row['URL_LINK'].'" />';
-
+		        echo '<input type="text" id="play_url" value="'.$row['URL_LINK'].'" style="width:175px; text-align:center; font-family: monospace; "/>';
                       return;
 		   };
                 };
                 echo '<input type="text" id="play_url" value="файл не найден" />';
  		return;
 };
-
 ?>
