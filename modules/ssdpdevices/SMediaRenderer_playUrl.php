eb8a8dc09af22f18844597f275adb367ceb226f7
tarasfrompir <tarasfrompir@inbox.ru>
2018-05-04 14:54:59 +0300

    Доработана функция Плей

--- modules/ssdpdevices/SMediaRenderer_playUrl.php
+++ modules/ssdpdevices/SMediaRenderer_playUrl.php
@@ -9,8 +9,22 @@ $renderer = new Renderer();
 $adress = $this->getProperty("CONTROLADDRESS");
 $remote = new Renderer\Remote($adress);
 $playUrl = $this->getProperty("playUrl");
+$info = $remote->getPosition();
+$doc = new \DOMDocument();
+$doc->loadXML($info);
+$result = $doc->getElementsByTagName('Track');
+$trackn = $result->item(0)->nodeValue;
+print ($trackn);       
+$result = $doc->getElementsByTagName('TrackURI');
+$trackurl = $result->item(0)->nodeValue;
+print ($trackurl); 
+$result = $doc->getElementsByTagName('RelTime');
+$tracktime = $result->item(0)->nodeValue;
+print ($tracktime); 
 if ( $playUrl ) {
+    $result = $remote->stop();
     $result = $remote->play($playUrl);
+    $result = $remote->setNext($trackurl);
     $this->setProperty("playUrl","");
 } else {
     $this->setProperty("playUrl","");
