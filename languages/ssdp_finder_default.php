4e0a9e5d6fbd11b692769ee893b6a92f1e4492e2
tarasfrompir <tarasfrompir@inbox.ru>
2018-07-09 12:28:59 +0300

    Update ssdp_finder_default.php

--- languages/ssdp_finder_default.php
+++ languages/ssdp_finder_default.php
@@ -13,6 +13,16 @@ $dictionary = array (
    'SSDP_TITLE' => 'Search and add deices to system devices on the UPNP protocol.',
    'SSDP_PORT' => 'Port for answer from UPNP devices',
    'SSDP_NAME' => 'Name:',
+   'SSDP_TYPE' => 'Type:',
+   'SSDP_SERVICETYPE' => 'Service Type:',
+   'SSDP_IPADRESS' => 'IP adress:',
+   'SSDP_IDESCRIPTION' => 'Description:',
+   'SSDP_MODEL' => 'Model:',
+   'SSDP_MANUFACTURER' => 'Manufacturer:',
+   'SSDP_CREATESD' => 'Create simple device',
+   'SSDP_HOSTONLINE' => 'Create hosts online',
+   'SSDP_TERMINAL' => 'Create terminal',
+   'SSDP_USETOSYSTEMMESAGE' => 'Use to play system mesage',
 );
 
 foreach ($dictionary as $k => $v) {
