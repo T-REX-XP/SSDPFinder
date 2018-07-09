c74bff2eb025419ca46ffb975fdf9fa10cfd5e48
tarasfrompir <tarasfrompir@inbox.ru>
2018-07-09 12:28:52 +0300

    Update ssdp_finder_ru.php

--- languages/ssdp_finder_ru.php
+++ languages/ssdp_finder_ru.php
@@ -13,8 +13,16 @@ $dictionary = array (
    'SSDP_TITLE' => 'Поиск и добавление в систему UPNP устройств.',
    'SSDP_PORT' => 'Порт для подписки на ответы от событий в устройствах',
    'SSDP_NAME' => 'Название:',
-
-
+   'SSDP_TYPE' => 'Тип:',
+   'SSDP_SERVICETYPE' => 'Типы Сервисов:',
+   'SSDP_IPADRESS' => 'IP адрес:',
+   'SSDP_DESCRIPTION' => 'Описание:',
+   'SSDP_MODEL' => 'Модель:',
+   'SSDP_MANUFACTURER' => 'Производитель:',
+   'SSDP_CREATESD' => 'Создать простое устройство',
+   'SSDP_HOSTONLINE' => 'Создать устройство онлайн',
+   'SSDP_TERMINAL' => 'Создать терминал устройства',
+   'SSDP_USETOSYSTEMMESAGE' => 'Использовать для воспроизведения системных сообщений',
 );
 
 foreach ($dictionary as $k => $v) {
