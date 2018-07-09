938ac1a097018f7f6c36f067bf29bd09a9976663
T-REX-XP <4560084+T-REX-XP@users.noreply.github.com>
2018-07-10 01:24:29 +0300

    Fix device logo loading. Fix device adding to result.

--- modules/ssdp_finder/ssdp_devices_scan.inc.php
+++ modules/ssdp_finder/ssdp_devices_scan.inc.php
@@ -36,69 +36,52 @@ if ($res[0]['UUID']) {
     $out['RESULT'] = $res;
 }
 
-function Scan()
-{
+function Scan(){
     $upnp = new Upnp();
     #print('searching...' . PHP_EOL);
     $everything = $upnp->discover();
     $result = [];
     $table_name='ssdp_devices';
 
-    foreach ($everything as $device) {
-        $control_url = str_ireplace("Location:", "", $device['location']);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $control_url);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $content = curl_exec($ch);
-        libxml_use_internal_errors(true); 
-        $xml = simplexml_load_string($content);
-        $uuid = $xml->device->UDN;
+    foreach ($everything as $deviceInfo) {
+
+        $device= $deviceInfo['description']['device'];
+
+       // print_r($device);
+        $uuid = $device["UDN"];
         $existed = SQLSelectOne("SELECT * FROM $table_name WHERE UUID='".$uuid."'");
-        // print array_search(, array_column( $result, 'ADDRESS'));
-        if (!array_search_result($result, 'CONTROLADDRESS', $control_url) && !is_null($uuid) && !($existed)) {
-            if (!$xml->device->presentationURL){
-                $presenturl='http://'.getIp($control_url,false);
-                } else {
-                $presenturl=$xml->device->presentationURL;
-                }
-            $presenturl = editLocalIp($presenturl);
-            $result[] = [
-                "ID" => $existed["ID"], //existed id Majordomo
-                "TITLE" => $xml->device->friendlyName,//friendly name
-                "ADDRESS" => $presenturl ,//presentation url (web UI of device),//presentation url (web UI of device)
-                "UUID" => $xml->device->UDN,
-                "DESCRIPTION" => $xml->device->modelDescription.$device['server'],//description get from xml or field "server"
-                "TYPE" => explode(":", $xml->device->deviceType)[3],//DeviceType
-                "LOGO" => getDefImg($control_url,$xml),//Logo 
-                "SERIAL" => $xml->device->serialNumber,//serialnumber
-                "MANUFACTURERURL" => $xml->device->manufacturerURL,//manufacturer url
-                "UPDATED" => '',
-                "MODEL" => $xml->device->modelName,//model
-                "MODELNUMBER" => $xml->device->modelNumber,//modelNumber
-                "MANUFACTURER" => $xml->device->manufacturer,//Manufacturer
-                "SERVICES"=> getServices($xml),//list services of device
-		"CONTROLADDRESS"=> editLocalIp($control_url),//list services of device
-            ];
-        }
+        $presenturl = $device["presentationURL"];
+        if (!array_search_result($result, 'UUID', $uuid) && !is_null($uuid) && !($existed)) {
+      // print($device["iconList"]);
+        $result[] = [
+            "ID" => $existed["ID"], //existed id Majordomo
+            "TITLE" => $device["friendlyName"],//friendly name
+            "ADDRESS" => $presenturl ,//presentation url (web UI of device),//presentation url (web UI of device)
+            "UUID" => $uuid,
+            "LOGO" => getDefImg($deviceInfo["location"],$device),//Logo 
+            "DESCRIPTION" => $device["modelDescription"] ,//. $deviceInfo['server'],//description get from xml or field "server"
+            "TYPE" => explode(":", $device["deviceType"])[3],//DeviceType
+            "SERIAL" => $device["serialNumber"],//serialnumber
+            "MANUFACTURERURL" => $device["manufacturerURL"],//manufacturer url
+            "UPDATED" => '',
+            "MODEL" => $device["modelName"],//model
+            "MODELNUMBER" => $device["modelNumber"],//modelNumber
+            "MANUFACTURER" => $device["manufacturer"],//Manufacturer
+            "SERVICES"=> getServices($device),//list services of device
+            "CONTROLADDRESS"=> editLocalIp($control_url),//list services of device
+        ];
     }
-    /*
-    print("<pre>");
-    print_r($result);
-     print("</pre>");
-    */
+    }
+
     return $result;
 }
 
-function array_search_result($array, $key, $value)
-{
-    //  global $result;
+function array_search_result($array, $key, $value){
     foreach ($array as $k => $v) {
         if (array_key_exists($key, $v) && ($v[$key] == $value)) {
             return true;
         }
     }
-    // return $result;;
 }
 
 
@@ -122,39 +105,42 @@ function getIp($baseUrl,$withPort) {
 
 //получаем hostname адрес локального компьютера
 function getLocalIp() { 
-return gethostbyname(trim(`hostname`)); 
+    return gethostbyname(trim(`hostname`)); 
 }
 
 // функция заменяет 127.0.0.1 на реальный IP адрес для локального компа
 function editLocalIp($baseUrl){ 
-$localIp=getLocalIp();
-$baseUrl = str_ireplace('127.0.0.1', $localIp, $baseUrl);
-return $baseUrl; 
+    $localIp=getLocalIp();
+    $baseUrl = str_ireplace('127.0.0.1', $localIp, $baseUrl);
+    return $baseUrl; 
 }
 
 
-function startsWith($haystack, $needle)
-{
+function startsWith($haystack, $needle){
      $length = strlen($needle);
      return (substr($haystack, 0, $length) === $needle);
 }
 
-function getServices($xml){
-	$result = array();
-	foreach($xml->device->serviceList->service as $type)
-    {
-        $name = explode(":", $type->serviceType)[3];
-		array_push($result,$name);
+function getServices($device){
+    $result = array();
+    if(isset($device["serviceList"]["service"]["serviceType"])){
+        $name = explode(":", $device["serviceList"]["service"]["serviceType"])[3];
+        array_push($result,$name);
+    }
+    else{
+        foreach($device["serviceList"]["service"] as $type)
+        {
+            $name = explode(":", $type["serviceType"])[3];
+            array_push($result,$name);
+        }
     }
 	return implode(",",$result);
 }
-function endsWith($haystack, $needle)
-{
+function endsWith($haystack, $needle){
 	return $needle === '' || substr_compare($haystack, $needle, -strlen($needle)) === 0;
 }
 
-function SearchArray($array, $searchIndex, $searchValue)
-{
+function SearchArray($array, $searchIndex, $searchValue){
     if (!is_array($array) || $searchIndex == '')
         return false;
 
@@ -167,30 +153,58 @@ function SearchArray($array, $searchIndex, $searchValue)
     return false;
 }
 
-function getDefImg($control_url,$xml) {
+
+function img64($icon){
+    return !is_null($icon) && $icon["width"] && $icon["url"] && $icon["width"] == 48;
+};
+
+function get_web_page( $url ) {
+    $res = array();
+    $options = array( 
+        CURLOPT_RETURNTRANSFER => true,     // return web page 
+        CURLOPT_HEADER         => false,    // do not return headers 
+        CURLOPT_FOLLOWLOCATION => true,     // follow redirects 
+        CURLOPT_USERAGENT      => "spider", // who am i 
+        CURLOPT_AUTOREFERER    => true,     // set referer on redirect 
+        CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect 
+        CURLOPT_TIMEOUT        => 120,      // timeout on response 
+        CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects 
+    ); 
+    $ch      = curl_init( $url ); 
+    curl_setopt_array( $ch, $options ); 
+    $content = curl_exec( $ch ); 
+    $err     = curl_errno( $ch ); 
+    $errmsg  = curl_error( $ch ); 
+    $header  = curl_getinfo( $ch ); 
+    curl_close( $ch ); 
+
+    $res['content'] = $content;     
+    $res['url'] = $header['url'];
+    return $res; 
+}
+
+
+function getDefImg($control_url,$device) {
+    $current="";
+    $type="";
+    $path="";
+    $url ="";
+ 
     $baseUrl = getIp($control_url,True);
-    $uuid = str_ireplace("uuid:", "",$xml->device->UDN);
-    if (!$xml->device->iconList->icon){
-	    return "/templates/ssdp_finder/img/".explode(":", $xml->device->deviceType)[3]. ".png";//"Icons not found
+    $icons =$device["iconList"]["icon"];
+    if (!$icons){
+	    return "/templates/ssdp_finder/img/".explode(":", $device["deviceType"])[3] . ".png";//"Icons not found
     } else {
-        foreach ($xml->device->iconList->icon as $icon) {
-	    if ($icon->with = 48){
-		$url = $icon->url;
-		break;
-	    } else if ($with < $icon->with) {
-		$url = $icon->url;
-		$with = $icon->with;
-	    } else {
-		$url = $icon->url;
-	    }
-	}  
+        if(isset($icons["url"])){
+            $url = $icons["url"];
+        }
+        else if(is_array($icons)){
+            $arrImg= array_filter($icons, "img64");
+            $url= isset($arrImg[0]["url"]) ? $arrImg[0]["url"]: $icons[0]["url"];
+        }
     }
-    if ((stristr($url, 'http') == True)) { //"Icons found in internet;
-        $current = file_get_contents($url);
-    } else {		
-        $current = file_get_contents($baseUrl.$url);
-    };
-    $link = '/templates/ssdp_finder/img/'.$uuid.'.png'; 
-    file_put_contents(ROOT.$link,  $current); // Save the image in local host
-    return $link;//"Icons found;
+    $path = $baseUrl . $url;
+    $current =get_web_page($path);
+    $type = pathinfo($path, PATHINFO_EXTENSION);
+    return 'data:image/' . $type . ';base64,' . base64_encode($current['content']);
 }
