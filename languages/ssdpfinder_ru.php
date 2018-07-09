07975d566d5ddb0c3accb539d86ce043dba19066
tarasfrompir <tarasfrompir@inbox.ru>
2018-07-09 11:02:28 +0300

    Create ssdpfinder_ru.php

---
+++ languages/ssdpfinder_ru.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @package MajorDoMo
+ * @author Serge Dzheigalo <jey@tut.by> http://smartliving.ru/
+ * @version 1.0
+ */
+
+
+
+$dictionary = array (
+
+
+
+);
