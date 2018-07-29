bf5fd7b32234a2283b17edf08c42a9f1b7e0981f
tarasfrompir <tarasfrompir@inbox.ru>
2018-07-29 22:38:26 +0300

    Add files via upload

--- modules/devices/addons/sspdfinder_structure.php
+++ modules/devices/addons/sspdfinder_structure.php
@@ -143,10 +143,14 @@ $this->device_types['RGBWSmartLight'] = array(
         'PROPERTIES'=>array(
             'turnOn'=>array('DESCRIPTION'=>'Включение', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'switch', 'DATA_KEY'=>1),
             'turnOff'=>array('DESCRIPTION'=>'Выключение', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'switch', 'DATA_KEY'=>1),
+			'changecolor'=>array('DESCRIPTION'=>'Изменить цвет', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'changecolor', 'DATA_KEY'=>1),
+			'changetemp'=>array('DESCRIPTION'=>'Изменить температуру', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'changetemp', 'DATA_KEY'=>1),
        ),
         'METHODS'=>array(
-            'turnOn'=>array('DESCRIPTION'=>'turnOn'),
-            'turnOff'=>array('DESCRIPTION'=>'turnOff'),
-            'switch'=>array('DESCRIPTION'=>'Switch'),
+            'turnOn'=>array('DESCRIPTION'=>'Включение'),
+            'turnOff'=>array('DESCRIPTION'=>'Выключение'),
+            'switch'=>array('DESCRIPTION'=>'Изменить состояние'),
+			'changecolor'=>array('DESCRIPTION'=>'Изменить цвет'),
+			'changetemp'=>array('DESCRIPTION'=>'Изменить температуру'),
         )
 );
