85b654b3342f7a3c4134f804972ec4000def081f
tarasfrompir <tarasfrompir@inbox.ru>
2018-07-29 22:51:13 +0300

    Add files via upload

--- modules/devices/addons/sspdfinder_structure.php
+++ modules/devices/addons/sspdfinder_structure.php
@@ -136,7 +136,7 @@ $this->device_types['controllee'] = array(
         )
 );
 
-$this->device_types['YelightSmartBulb'] = array(
+$this->device_types['YeelightSmartBulb'] = array(
         'TITLE'=>'Yelight лампа',
         'PARENT_CLASS'=>'UPNPdevices',
         'CLASS'=>'Yelight',
