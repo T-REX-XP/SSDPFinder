b72993733ad7d87b61ffc175129bf716683c3600
tarasfrompir <tarasfrompir@inbox.ru>
2018-07-30 12:43:54 +0300

    Add files via upload

--- modules/devices/SYeelightSmartBulb_turnOff.php
+++ modules/devices/SYeelightSmartBulb_turnOff.php
@@ -1,11 +1,10 @@
 <?php
-require(dirname(__FILE__).'/../ssdp_finder/upnp/vendor/autoload.php');
-use jalder\Upnp\BelkinDevice;
-$BelkinDevice= new BelkinDevice();
+require(dirname(__FILE__).'/../ssdp_finder/upnp/vendor/jalder/upnp/src/Yeelight/Yeelight.class.php');
 $adress = $this->getProperty("CONTROLADDRESS");
 $status = $this->getProperty("turnOff");
-$remote = new BelkinDevice\Devswitch($adress);
-$result = $remote->off();
+
+$yee = new Yeelight($adress);
+$result = $yee->set_power("off"); // power off
 //print_r($result);
 if ($result) {
     $this->setProperty('status', 0);
@@ -14,3 +13,24 @@ if ($result) {
     $this->setProperty('alive', 0);
     say ("Выключатель ".$this->object_title." размещенный в комнате ".$this->getProperty("linkedRoom")." не сработал!", 2);
 };
+$yee->disconnect();
+
+
+//<?php
+//require "Yeelight.class.php";
+
+//$yee = new Yeelight("10.0.0.201", 55443);
+
+//$yee->set_power("on"); // power on
+//$yee->set_rgb(0xFF0000); // color to red
+//$yee->set_bright(50); // brightness to 50%
+
+//$yee->commit(); // changes are not sent to the bulb before commit() is called
+
+//sleep(10);
+//$yee->set_rgb(0x00FF00)->set_bright(100)->commit(); // calls return the object for fast chaining of commands
+
+//$status = $yee->get_prop("power")->commit(); // get current status
+//print_r($status);
+
+//$yee->disconnect();
