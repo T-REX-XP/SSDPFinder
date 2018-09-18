10c9a306adbbd397e93466c85f57bfaf012d2ed7
tarasfrompir <tarasfrompir@inbox.ru>
2018-09-18 21:19:21 +0300

    Update Core.php

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
@@ -45,7 +45,7 @@ class Core {
         // search device of you PC
         socket_sendto($socket, $request, strlen($request), 0, '255.255.255.255', 1900);
         // search device of other net
-        //socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 1900);
+        socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 1900);
 
         // send the data from socket
         socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec'=>$sockTimout, 'usec'=>'128'));
