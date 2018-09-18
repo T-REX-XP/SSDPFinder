d90ca8aee8abeb4c6913087a71017746bce112b3
tarasfrompir <tarasfrompir@inbox.ru>
2018-09-18 11:32:41 +0300

    Update extended_modules.php

--- modules/ssdp_finder/extended_modules.php
+++ modules/ssdp_finder/extended_modules.php
@@ -3,4 +3,8 @@
         'YeelightSmartBulb'=>'Yeelight', 
         'MagicHome'=>'magichome',
         'MiLight'=>'MiLight',
+        'urn:schemas-sony-com:service:IRCC:1'=>'sonytv',
+
 );
+
+// https://connect.smartliving.ru/market/?lang=ru&serial=&locale=ru&os=win
