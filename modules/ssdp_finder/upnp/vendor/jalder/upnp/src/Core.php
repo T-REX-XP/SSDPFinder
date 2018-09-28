d6ce18c5ebc4d884aad406a766ae8604c60f204c
tarasfrompir <tarasfrompir@inbox.ru>
2018-09-28 16:01:36 +0300

    Add files via upload

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
@@ -19,20 +19,6 @@ class Core {
     	$socket = socket_create(AF_INET, SOCK_DGRAM, 0);
         socket_set_option($socket, SOL_SOCKET, SO_BROADCAST, true);
 
-        // поиск устройств milight, MagicHome
-        //$request = 'HF-A11ASSISTHREAD'."\r\n";
-        //socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 48899);		
-
-
-        //поиск устройств yeelight
-        //$request = 'M-SEARCH * HTTP/1.1'."\r\n";
-        //$request .= 'HOST: 239.255.255.250:1982'."\r\n";
-        //$request .= 'MAN: "'.$man.'"'."\r\n";
-        //$request .= 'MX: '.$mx.''."\r\n";
-        //$request .= 'ST: wifi_bulb'."\r\n";
-        //$request .= "\r\n";
-        //socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 1982);		
-
         //all
         $request = 'M-SEARCH * HTTP/1.1'."\r\n";
         $request .= 'HOST: 239.255.255.250:1900'."\r\n";
@@ -42,10 +28,62 @@ class Core {
         $request .= 'USER-AGENT: '.$this->user_agent."\r\n";
         $request .= "\r\n";
 		
-        // search device of you PC
-//        socket_sendto($socket, $request, strlen($request), 0, '255.255.255.255', 1900);
-        // search device of other net
         socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 1900);
+
+        // send the data from socket
+        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec'=>$sockTimout, 'usec'=>'128'));
+        $response = array();
+        do {
+            $buf = null;
+            if (($len = @socket_recvfrom($socket, $buf, 2048, 0, $ip, $port)) == -1) {
+                echo "socket_read() failed: " . socket_strerror(socket_last_error()) . "\n";
+            }
+            if(!is_null($buf)){
+                $data = $this->parseSearchResponse($buf);
+                $response[$data['usn']] = $data;
+            }
+        } while(!is_null($buf));
+        socket_close($socket);
+
+        return $response;
+    }
+
+   
+    public function search_3rddevice($st = 'ssdp:all', $mx = 2, $man = 'ssdp:discover', $from = null, $port = null, $sockTimout = '2')
+    {
+        //create the socket
+    	$socket = socket_create(AF_INET, SOCK_DGRAM, 0);
+        socket_set_option($socket, SOL_SOCKET, SO_BROADCAST, true);
+
+        // поиск устройств milight, MagicHome
+        $request = 'HF-A11ASSISTHREAD'."\r\n";
+        socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 48899);		
+
+        // seech milo device
+        $request = hex2bin('21310020ffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
+        socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 54321);		
+
+
+        //поиск устройств yeelight
+        $request = 'M-SEARCH * HTTP/1.1'."\r\n";
+        $request .= 'HOST: 239.255.255.250:1982'."\r\n";
+        $request .= 'MAN: "'.$man.'"'."\r\n";
+        $request .= 'MX: '.$mx.''."\r\n";
+        $request .= 'ST: wifi_bulb'."\r\n";
+        $request .= "\r\n";
+        socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 1982);		
+
+         //all
+        $request = 'M-SEARCH * HTTP/1.1'."\r\n";
+        $request .= 'HOST: 239.255.255.250:1900'."\r\n";
+        $request .= 'MAN: "'.$man.'"'."\r\n";
+        $request .= 'MX: '.$mx.''."\r\n";
+        $request .= 'ST: '.$st.''."\r\n";
+        $request .= 'USER-AGENT: '.$this->user_agent."\r\n";
+        $request .= "\r\n";
+		
+        // search device of you PC
+        socket_sendto($socket, $request, strlen($request), 0, '255.255.255.255', 1900);
 
         // send the data from socket
         socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec'=>$sockTimout, 'usec'=>'128'));
