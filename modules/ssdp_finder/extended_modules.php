d2b1e841d5170e9a320cd1b739bd5b3db20107c2
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-02 15:17:54 +0300

    Update extended_modules.php

--- modules/ssdp_finder/extended_modules.php
+++ modules/ssdp_finder/extended_modules.php
@@ -5,6 +5,7 @@
         'MiLight'=>'MiLight',
         'urn:schemas-sony-com:service:IRCC:1'=>'sonytv',
         'STB DeviceServices'=>'mag250',
+        'miIO protocol device'=>'xiaomimiio',
 );
 
 // https://connect.smartliving.ru/market/?lang=ru&serial=&locale=ru&os=win
