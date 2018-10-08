be17936f59fbb2d523870aa71f43aa82f34c30f9
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-08 19:55:32 +0300

    Update Core.php

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
@@ -74,24 +74,11 @@ class Core {
 //фунция поиска ONVIF устройств
 private function search_ONVIF($sockTimout = '2') {
     $response = array();
-    $timeout = time() + $sockTimout;
-    $post_string = '<?xml version="1.0" encoding="UTF-8"?><e:Envelope xmlns:e="http://www.w3.org/2003/05/soap-envelope" xmlns:w="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:d="http://schemas.xmlsoap.org/ws/2005/04/discovery" xmlns:dn="http://www.onvif.org/ver10/network/wsdl"><e:Header><w:MessageID>uuid:84ede3de-7dec-11d0-c360-f01234567890</w:MessageID><w:To e:mustUnderstand="true">urn:schemas-xmlsoap-org:ws:2005:04:discovery</w:To><w:Action a:mustUnderstand="true">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</w:Action></e:Header><e:Body><d:Probe><d:Types>dn:NetworkVideoTransmitter</d:Types></d:Probe></e:Body></e:Envelope>';
-	$sock = @socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-	socket_bind($sock, 0, rand(20000, 40000));
-	socket_set_option($sock, IPPROTO_IP, MCAST_JOIN_GROUP, array('group' => '239.255.255.250'));
-	socket_sendto($sock, $post_string, strlen($post_string), 0, '239.255.255.250', 3702);
-	socket_set_nonblock($sock);
-    do {
-            $buf = null;
-            if (($len = @socket_recvfrom($socket, $buf, 9999, 0, $ip, $port)) == -1) {
-                echo "socket_read() failed: " . socket_strerror(socket_last_error()) . "\n";
-            }
-            if(!is_null($buf)){
-                $data = $this->parseSearchResponse($buf);
-                $response[$data['usn']] = $buf;
-            }
-        } while(!is_null($buf));
-        socket_close($sock);
+    require 'scanonvif.php';
+
+    $onvif = new scanonvif();
+    $response = $onvif->discover();
+
     return $response;
     }
 	
