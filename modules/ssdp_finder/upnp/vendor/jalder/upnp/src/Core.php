cf9a798dbbf461a5631558b6c9c550ad80d1bd11
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-09 23:13:49 +0300

    Update Core.php

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
@@ -55,15 +55,14 @@ class Core {
 		
 	// сканируем магикхом устройства отдельно
         $mghome = $this->search_MAGICHOME($sockTimout = '2');
-	$response = array_merge($response, $mghome);
         
 	// сканируем ксяоми устройства отдельно
         //$xyaomi = $this->search_XYAOMIDEVICES($sockTimout = '2');
 	//$response = array_merge($response, $xyaomi);
         
 	// сканируем ксяоми устройства отдельно
-        //$mag250 = $this->search_MAG250($sockTimout = '2');
-	//$response = array_merge($response, $mag250);
+        $mag250 = $this->search_MAG250($sockTimout = '2');
+	$response = array_merge($response, $mag250, $mghome);
         
 	// сканируем ксяоми устройства отдельно
         //$onvif = $this->search_ONVIF($sockTimout = '2');
