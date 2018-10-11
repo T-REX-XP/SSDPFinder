62133d5deddc4294a69eb3cca0dbba26e8142063
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-11 16:14:54 +0300

    Update 3rddevice_devices_scan.inc.php

--- modules/ssdp_finder/3rddevice_devices_scan.inc.php
+++ modules/ssdp_finder/3rddevice_devices_scan.inc.php
@@ -67,7 +67,7 @@ function Scan_3rddevice()
                 $existed = SQLSelectOne("SELECT * FROM $table_name WHERE UUID='" . $uuid . "'");
 
                 // need for chek device type
-                $device_type = 'BROADLINK protocol device'; //DeviceType
+                $device_type = 'broadlink'; //DeviceType
                 $services = 'BROADLINK protocol device'; //DeviceServices
                 // проверяем на наличие модуля в системе
                 $mod_cheked = SQLSelectOne("SELECT * FROM project_modules WHERE NAME LIKE '" . $modules['broadlink'] . "'");
