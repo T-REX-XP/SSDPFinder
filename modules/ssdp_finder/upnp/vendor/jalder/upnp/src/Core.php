01d9dcd8fd8fd000abfaf5c3cb468d5400b47708
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-14 12:19:56 +0300

    Update Core.php

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Core.php
@@ -97,7 +97,6 @@ private function search_ONVIF($sockTimout = '2') {
 			while(time() < $timeout){
 				if(FALSE !== @socket_recvfrom($sock, $response, 9999, 0, $ip, $port)){
 					if($response != NULL && $response != $post_string){
-						$response = $this->_xml2array($response);
 						DebMes($response);
 					}
 				}
@@ -747,25 +746,4 @@ private function getmodel($devtype){
 
 	        return $model;
     }	
-	// onvif added function
-	private function _xml2array($response) {
-		$sxe = new SimpleXMLElement($response);
-		$dom_sxe = dom_import_simplexml($sxe);
-		$dom = new DOMDocument('1.0');
-		$dom_sxe = $dom->importNode($dom_sxe, true);
-		$dom_sxe = $dom->appendChild($dom_sxe);
-		$element = $dom->childNodes->item(0);
-		foreach ($sxe->getDocNamespaces() as $name => $uri) {
-    			$element->removeAttributeNS($uri, $name);
-		}
-		$xmldata=$dom->saveXML();
-		$xmldata=substr($xmldata,strpos($xmldata,"<Envelope>"));
-		$xmldata=substr($xmldata,0,strpos($xmldata,"</Envelope>")+strlen("</Envelope>"));
-		$xml=simplexml_load_string($xmldata);
-		$data=json_decode(json_encode((array)$xml),1);
-		$data=array($xml->getName()=>$data);
-		return $data;
-	}
-
-
 }
